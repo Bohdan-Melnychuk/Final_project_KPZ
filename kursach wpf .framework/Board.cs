5cd5da61c1cf6b0420c501bb8a6bcef0534454d6
Melnychuk Bohdan <ipz243_mba@student.ztu.edu.ua>
2025-05-13 21:50:46 +0300

    Фікс бібліотек.

--- kursach wpf .framework/Board.cs
+++ kursach wpf .framework/Board.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
